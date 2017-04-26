f343901c6f71bbb490fa7542e7d87fdf4f1cefe6
Daniel Serratore <Daniel Serratore>
2017-04-26 13:52:00 -0500

    Added sorting by stream status. Added streams now only load on page reload.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,33 @@
-tracked_streams = [];
+var promises = [];
+var streams = [];
 $(document).ready(function(){
+	// Retrieve saved streams and add them.
+	for (var i = 0; i < localStorage.length; i++){
+		getData(localStorage.key(i));
+	}
+
+	// Default streams.
 	getData("destiny");
 	getData("monstercat");
 	getData("1234567890123456789012");
 
+	// When all requests resolve, add online streams first, then offline streams.
+	$.when.apply($, promises).done(function(){
+		// INEFFICIENT
+		streams.forEach(function(stream){
+			if (stream.status == "online"){
+				setPage(stream);
+			}
+		});
+		streams.forEach(function(stream){
+			if (stream.status == "offline"){
+				setPage(stream);
+			}
+		});
+	});
+
+
+	// <-- EVENT HANDLERS -->
 	// "Add stream" button on page.
 	$("#new").on("click", function(){
 		$("#myModal").modal('show');
@@ -13,6 +37,9 @@ $(document).ready(function(){
 	$("#add").on("click", function(){
 		// Get input.
 		var streamer = $("#stream-add").val();
+
+		// Add stream to list.
+		localStorage.setItem(streamer, "0");
 		getData(streamer);
 
 		// Clean.
@@ -25,6 +52,9 @@ $(document).ready(function(){
 		if(e.which == 13){
 		// Get input.
 		var streamer = $(this).val();
+
+		// Add stream to list.
+		localStorage.setItem(streamer, "0");
 		getData(streamer);
 
 		// Clean.
@@ -39,42 +69,51 @@ function getData(name){
 	url = `${endpoint}${name}?callback=?`; // URL query.
 
 	// Add data to page.
-	jQuery.getJSON(url, function(json){
-		
+	promises.push(jQuery.getJSON(url, function(json){
 		if(json.stream != null){ // If stream is online.
-			// var game = json.stream.game.length <= 14 ? json.stream.game : json.stream.game.slice(0,12) + "...";
-
-			// Shorten titles with length > 75 or unspaced length > 35. 
-			// var status = (json.stream.channel.status.length <= 75 && json.stream.channel.status.split(" ").sort(function(a,b){return a.length - b.length})[0].length <= 35 ? json.stream.channel.status : json.stream.channel.status.slice(0,32) + "...");
-
-			// Build Page.
-			$(".row").append(`
-				<div class="col-lg-4">
-					<div class="card" style="background-image: url(${json.stream.preview.large}); background-size: cover;" onclick="parent.open('http://twitch.tv/${name}')">
-						<div class="card-block" style="background-color: rgba(103,58,183,0.5);"> 
-							<div class="card-top title">
-								<h3 class="card-title text-left">${name}</h3>
-							</div>
-							<div class="card-top subtitle">
-								<p class="card-subtitle text-left"><strong>Viewers</strong>: ${json.stream.viewers}</p>
-								<p class="card-subtitle text-left"><strong>Game</strong>: ${json.stream.game}</p>
-							</div>
-							<p><strong>Streaming: </strong>${json.stream.channel.status}</p>
-						</div>
-					</div>
-				</div>`);
+			streams.push({
+				user: name,
+				status: "online",
+				viewers: json.stream.viewers,
+				game: json.stream.game,
+				title: json.stream.channel.status,
+				preview: json.stream.preview.large});
 		}
-		
-		else{ // If stream is offline.
-			$(".row").append(`
-				<div class="col-lg-4">
-					<div class="card">
-						<div class="card-block" style="background-color: rgb(50,50,50);">
-							<h3 class="card-title text-left">${name}</h3>
-							<p id="offline"><strong>Offline</strong></p>
-						</div>
-					</div>
-				</div>`);	
+		else{ // If stream is offline.	
+			streams.push({
+				user: name,
+				status: "offline"});
 		}
-	});
+	}));
+}
+
+function setPage(stream){
+	if (stream.status == "online"){
+		$(".row").append(`
+			<div class="col-lg-4">
+				<div class="card" style="background-image: url(${stream.preview}); background-size: cover;" onclick="parent.open('http://twitch.tv/${stream.name}')">
+					<div class="card-block" style="background-color: rgba(103,58,183,0.5);"> 
+						<div class="card-top title">
+							<h3 class="card-title text-left">${stream.user}</h3>
+						</div>
+						<div class="card-top subtitle">
+							<p class="card-subtitle text-left"><strong>Viewers</strong>: ${stream.viewers}</p>
+							<p class="card-subtitle text-left"><strong>Game</strong>: ${stream.game}</p>
+						</div>
+						<p><strong>Streaming: </strong>${stream.title}</p>
+					</div>
+				</div>
+			</div>`);
+	}
+	else{
+		$(".row").append(`
+			<div class="col-lg-4">
+				<div class="card">
+					<div class="card-block" style="background-color: rgb(50,50,50);">
+						<h3 class="card-title text-left">${stream.user}</h3>
+						<p id="offline"><strong>Offline</strong></p>
+					</div>
+				</div>
+			</div>`);
+	}
 }
